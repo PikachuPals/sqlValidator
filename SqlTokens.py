4420359b66489378a27cec002b346b3d40c5c79b
Andy Lau <pikachupalsmail@gmail.com>
2021-03-07 23:52:49 +0000

    Minor changes non temp

--- SqlTokens.py
+++ SqlTokens.py
@@ -1,17 +1,19 @@
 import sqlparse
 from sqlparse.tokens import *
 from sqlparse.sql import *
-from sqlparse import tokens as T
 
 def identifierTokens(tokens):
     idTokens = []
     
     for token in tokens:
+        
         if isinstance(token, Identifier):
             idTokens.append(token)
+        
         elif isinstance(token, IdentifierList):
             for identifier in token.get_identifiers():
                 idTokens.append(identifier)
+                
         elif isinstance(token, Function):
             for identifier in Function.get_parameters(token):
                 idTokens.append(identifier)
