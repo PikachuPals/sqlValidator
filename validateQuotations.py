813fe038ad281f6d20062e83722fbc652a835821
Andy Lau <pikachupalsmail@gmail.com>
2021-03-07 23:53:24 +0000

    Design actually might work.

--- validateQuotations.py
+++ validateQuotations.py
@@ -2,24 +2,42 @@ import sqlparse
 from sqlparse.tokens import *
 from sqlparse.sql import *
 import re
+
 import outputInterface as OI
-
 import SqlTokens as retrieveTokens
+from Reasoning import reason
 
-def checkQuotations(sqlTokens):
+def checkQuotations(validator):
+    sqlTokens = validator.tokens
+
+    # Iterate to where and find wrong identifiers.
+    idTokens = []
+
+    for token in sqlTokens:
+        if isinstance(token, Where):
+            break
+        
+        elif isinstance(token, IdentifierList):
+            for identifier in token.get_identifiers():
+                misQuotedSelectIdentifier(identifier)
+                
+            idTokens.append(identifier)
+            
+        else:
+            misQuotedSelectIdentifier(token)    
+    
     whereTokens = retrieveTokens.whereTokens(sqlTokens)
-    idTokens = retrieveTokens.identifierTokens(sqlTokens)
+
 
     for token in whereTokens:
         if isinstance(token, Comparison):
-            checkIdentifiers(token.tokens[0])
-        
+            misQuotedSelectIdentifier(token.tokens[0])
+       
 
-def checkIdentifiers(identifier):
+def misQuotedSelectIdentifier(token):
+    tokenString = token.value
+    if tokenString.startswith("'") and tokenString.endswith("'"):
+        OI.cmdLineOutput(tokenString, "test", reason["misQuotedSelectIdentifier"])
 
-    if identifier.value.startswith("'") and identifier.value.endswith("'"):
-        return False
-    else:
-        return True
     
     
