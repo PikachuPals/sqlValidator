78d521a70b4d6968a3c1b6e64d6878bacb068bbc
Andy Lau <pikachupalsmail@gmail.com>
2021-03-12 00:12:50 +0000

    Altered messages

--- Reasoning.py
+++ Reasoning.py
@@ -1,3 +1,3 @@
 reason = {
-    "misQuotedSelectIdentifier": "Possible use of wrong quotations. If intention is column referencing, use " + '""' + "instead of ''",
+    "misQuotedSelectIdentifier": "Possible use of wrong quotations. If intention is column referencing, wrap column with " + '"' + " instead of '.",
     }
