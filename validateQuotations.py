245c59de5cc8a893d69a6873bcfe24cc0c47be62
Andy Lau <pikachupalsmail@gmail.com>
2021-04-14 18:32:31 +0100

    Fixed and rearranged logic.

--- validateQuotations.py
+++ validateQuotations.py
@@ -14,36 +14,62 @@ def checkQuotations(validator):
     tokenIndex = 0
 
     for token in sqlTokens:
-        
+
         if isinstance(token, Where):
-            
+
             for innerToken in token:
                 if isinstance(innerToken, Comparison):
                     resolver = misQuotedSelectIdentifier(token.tokens[0], tokenIndex, validator)
+
                     if resolver is not None:
                         resolver.whereLeftChange()
-                    
+
             tokenIndex += 1
-        
+
         elif isinstance(token, IdentifierList):
             for identifier in token.get_identifiers():
-                misQuotedSelectIdentifier(identifier)
-             
-        else:
-            misQuotedSelectIdentifier(token, tokenIndex, validator)
+                resolver = misQuotedSelectIdentifier(identifier, tokenIndex, validator)
 
-        tokenIndex += 1 
+                if resolver is not None:
+                    resolver.topLevelChange()
+
+                if identifier.has_alias():
+                    resolver = misQuotedAlias(token.tokens[0], tokenIndex, validator)
+                    if resolver is not None:
+                        resolver.aliasChange()
+
+        else:
+            resolver = misQuotedSelectIdentifier(token, tokenIndex, validator)
+            if resolver is not None:
+                resolver.topLevelChange()
+
+        tokenIndex += 1
 
 def misQuotedSelectIdentifier(token, tokenIndex, validator):
     tokenString = token.value
-    
+
     if tokenString.startswith("'") and tokenString.endswith("'"):
         tokenString = '"' + tokenString[1: -1] + '"'
-        
+
         return sqlResolver(validator, tokenIndex, tokenString, reason["misQuotedSelectIdentifier"])
 
     else:
         return None
-    
-    
-    
+
+def misQuotedAlias(token, tokenIndex, validator):
+    aliasRegex = r"(?i)(?<=as )(.+$)"
+
+    aliasMatch = re.match(aliasRegex, token)
+    alias = aliasMatch.group(0)
+
+    if alias.startswith("'") and alias.endswith("'"):
+        alias = '"' + alias[1: -1] + '"'
+
+        return sqlResolver(validator, tokenIndex, alias, reason["misQuotedAlias"])
+
+    elif " " in alias:
+        if not alias.startswith('"') or alias.endswith('"'):
+            alias = '"' + alias + '"'
+
+            return sqlResolver(validator, tokenIndex, alias, reason["aliasMissingQuotes"])
+    pass
