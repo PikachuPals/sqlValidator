b877a58e185e7be386c1a0c1303390bbd4bed693
Andy Lau <pikachupalsmail@gmail.com>
2021-04-14 18:31:23 +0100

    Added more

--- Reasoning.py
+++ Reasoning.py
@@ -1,3 +1,5 @@
 reason = {
     "misQuotedSelectIdentifier": "Possible use of wrong quotations. If intention is column referencing, wrap column with " + '"' + " instead of '.",
+    "misQuotedAlias": "Aliases should be wrapped with  " + '"' + " instead of '.",
+    "aliasMissingQuotes": "If you want to use spaces in your alias name, wrap with ''."
     }
