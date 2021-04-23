456940da44141836a0da1d664bad4cac519bc0a7
Andy Lau <pikachupalsmail@gmail.com>
2021-04-23 13:30:53 +0100

    More dynamic code to differentiate between tokens

--- SqlResolver.py
+++ SqlResolver.py
@@ -22,7 +22,7 @@ class sqlResolver:
         errorInStatement = underlineText(placeholderParse.tokens[self.tokenIndex].value)
         placeholderParse.tokens[self.tokenIndex].value = errorInStatement
 
-        if OI.cmdLineOutput(placeholderParse, self.change, self.reason):
+        if OI.output(placeholderParse, self.change, self.reason):
             newToken = sqlparse.parse(self.change)[0].tokens[0]
             self.validator.alterQuery(self.tokenIndex, newToken)
 
@@ -31,15 +31,28 @@ class sqlResolver:
         placeholderParse = self.validator.getParsed()
 
         # Underline error in text and parse into tokens.
-        errorInStatement = underlineText(placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].value)
-
         # Rewrite old token with newer token.
+        errorInStatement = underlineText(placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].value)
         placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].value = errorInStatement
 
-        if OI.cmdLineOutput(placeholderParse, self.change, self.reason):
+        if hasattr(placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex], 'tokens'):
+            errorInStatement = underlineTokenList(placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].tokens)
+            placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].tokens = errorInStatement
+        else:
+            errorInStatement = underlineText(placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].value)
+            placeholderParse.tokens[self.tokenIndex].tokens[self.innerIndex].value = errorInStatement
+
+        if OI.output(placeholderParse, self.change, self.reason):
             newInnerToken = sqlparse.parse(self.change)[0].tokens[0]
             self.validator.getParsed().tokens[self.tokenIndex].tokens[self.innerIndex]
             self.validator.alterQueryInnerToken(self.tokenIndex, self.innerIndex, newInnerToken)
+
+    # Function to choose between token changes if innerIndex is set.
+    def dynamicTokenChange(self):
+        if self.innerIndex is not None:
+            self.innerTokenRootChange()
+        else:
+            self.rootChange()
 
     def innerTokenChange(self):
         pass
@@ -57,3 +70,9 @@ class sqlResolver:
 
 def underlineText(text):
     return u"\u001b[4m" + text + "\u001b[0m"
+
+def underlineTokenList(tokenList):
+    for token in tokenList:
+        token.value = underlineText(token.value)
+
+    return tokenList
