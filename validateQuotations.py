1d9a27168a2cd22d5c8f9780f9768e45ee8bfa93
Andy Lau <pikachupalsmail@gmail.com>
2021-04-23 13:32:01 +0100

    Major changes and refactoring for outputs and tokens.

--- validateQuotations.py
+++ validateQuotations.py
@@ -15,13 +15,23 @@ def checkQuotations(validator):
 
     for token in sqlTokens:
 
+        # Misquoted Identifiers appear as string literals.
         if token.ttype == sqlparse.tokens.Token.Literal.String.Single:
             misQuotedSelectIdentifier(token, tokenIndex, validator)
 
+        # If multiple identifiers, tokens get collected into an identifier list.
         elif isinstance(token, IdentifierList):
             for innerIndex, identifier in enumerate(token.tokens):
                 if identifier.ttype == sqlparse.tokens.Token.Literal.String.Single:
                     misQuotedInnerIdentifier(identifier, tokenIndex, innerIndex, validator)
+
+                elif isinstance(identifier, Identifier):
+                    if identifier.is_group:
+                        misQuotedAlias(identifier, tokenIndex, validator, innerIndex)
+
+        elif isinstance(token, Identifier):
+            if token.is_group:
+                misQuotedAlias(token, tokenIndex, validator)
 
         tokenIndex += 1
 
@@ -44,25 +54,41 @@ def misQuotedInnerIdentifier(token, tokenIndex, innerIndex, validator):
         resolver = sqlResolver(validator, tokenIndex, tokenString, reason["misQuotedSelectIdentifier"], innerIndex)
         resolver.innerTokenRootChange()
 
+def misQuotedAlias(token, tokenIndex, validator, innerIndex = None):
+    tokenValue = token.value
 
+    aliasRegex = r"(?i)(?<= as )(.+$)"
+    aliasMatch = re.search(aliasRegex, tokenValue)
 
-def misQuotedAlias(token, tokenIndex, validator):
-    aliasRegex = r"(?i)(?<=as )(.+$)"
+    if aliasMatch is not None:
+        indices = aliasMatch.span()
+        alias = aliasMatch.group(0)
 
-    aliasMatch = re.search(aliasRegex, token.value)
-    alias = aliasMatch.group(0)
+        if alias.startswith("'") and alias.endswith("'"):
+            alias = '"' + alias[1: -1] + '"'
+            tokenValue = tokenValue[:indices[0]] + alias + tokenValue[indices[1]:]
 
-    if alias.startswith("'") and alias.endswith("'"):
-        alias = '"' + alias[1: -1] + '"'
+            resolver = sqlResolver(validator, tokenIndex, tokenValue, reason["misQuotedAlias"], innerIndex)
+            resolver.dynamicTokenChange()
 
-        return sqlResolver(validator, tokenIndex, alias, reason["misQuotedAlias"])
+        elif " " in alias:
+            if not alias.startswith('"') or alias.endswith('"'):
+                alias = '"' + alias + '"'
+                tokenValue = tokenValue[:indices[0]] + alias + tokenValue[indices[1]:]
 
-    elif " " in alias:
-        if not alias.startswith('"') or alias.endswith('"'):
-            alias = '"' + alias + '"'
+                resolver = sqlResolver(validator, tokenIndex, tokenValue, reason["aliasMissingQuotes"], innerIndex)
+                resolver.dynamicTokenChange()
 
-            return sqlResolver(validator, tokenIndex, alias, reason["aliasMissingQuotes"])
-    pass
+    identifierRegex = r"(?i)(.+)(?= as )"
+    identifierMatch = re.search(identifierRegex, tokenValue)
 
+    if identifierMatch is not None:
+        indices = identifierMatch.span()
+        identifier = identifierMatch.group(0)
 
-# TODO Check entire string, then generate correct alias and identifier.
+        if identifier.startswith("'") and identifier.endswith("'"):
+            identifier = '"' + identifier[1: -1] + '"'
+            tokenValue = tokenValue[:indices[0]] + identifier + tokenValue[indices[1]:]
+
+            resolver = sqlResolver(validator, tokenIndex, tokenValue, reason["misQuotedSelectIdentifier"], innerIndex)
+            resolver.dynamicTokenChange()
