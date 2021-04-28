549b275a8c99f464e23e6273e5517b755acc705d
Andy Lau <pikachupalsmail@gmail.com>
2021-04-28 19:19:18 +0100

    File for validation

---
+++ validateTableAlias.py
@@ -0,0 +1,10 @@
+import sqlparse
+from sqlparse.tokens import *
+from sqlparse.sql import *
+import re
+
+from SqlResolver import sqlResolver
+from Reasoning import reason
+
+def checkTableAlias(validator):
+    pass
