22d98d0f1eeac960cc6a1ebed299cf1c172288b6
Andy Lau <pikachupalsmail@gmail.com>
2021-04-30 00:05:04 +0100

    Removed fluff

--- SqlMain.py
+++ SqlMain.py
@@ -1,58 +1,13 @@
 import re
 from SqlValidator import sqlValidator
+from validatorCreator import createValidators
 
 def queryIntake():
     query = input("Enter SQL Query: ")
     return query
 
-def identifySubQueries(query):
-    openingParenthesis = list()
-    subQueries = {}
-
-    subre = re.compile("(?i)\(\s*(select)")
-
-    for index in range(0, len(query)):
-        character = query[index]
-
-        if character == "(":
-            openingParenthesis.append(index)
-
-        elif character == ")":
-            if not openingParenthesis:
-                print("Opening Parenthesis Missing")
-
-            else:
-                startIndex = openingParenthesis.pop()
-                if subre.match(query[startIndex:index + 1]) is not None:
-                    subQueries[startIndex] = index + 1
-
-    return subQueries
-
-def findAll(searchString, query):
-    index = query.find(searchString)
-    while index != -1:
-        yield index
-        index = query.find(searchString, index + 1)
-
 inputQuery = queryIntake()
 
-subQueries = identifySubQueries(inputQuery)
+createValidators(inputQuery)
 
-queryVariables = {}
-dynVar = 0
-diff = 0
-
-for k, v in subQueries.items():
-
-    startIndex = int(k) - diff
-    endIndex = int(v) - diff
-
-    queryVariables[dynVar] = sqlValidator(inputQuery[startIndex + 1:endIndex - 1], True)
-    inputQuery = inputQuery.replace(inputQuery[startIndex:endIndex], "{" + str(dynVar) + "}", 1)
-
-    diff += (v - k - 3)
-    dynVar += 1
-
-mainQuery = sqlValidator(inputQuery)
-print(mainQuery.query)
 #SELECT SalesOrderID, LineTotal,(SELECT AVG(LineTotal) FROM Sales.SalesOrderDetail) AS AverageLineTotal, LineTotal - (SELECT AVG(LineTotal) FROM Sales.SalesOrderDetail) AS Variance FROM Sales.SalesOrderDetail
